a1e9bda5cdca3a6a18d34045bffddbb7a3c967b0
VincentB <vinc.brocheton@gmail.com>
2023-05-20 11:46:59 +0200

    Gestion Assert Violation in Competence Class

--- src/Entity/Competence.php
+++ src/Entity/Competence.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CompetenceRepository::class)]
 #[ApiResource]
@@ -19,9 +20,15 @@ class Competence
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Le nom de la compétence ne peut pas être vide')]
+    #[Assert\Length(min: 3, max: 255,minMessage: 'Le nom de la compétence doit contenir au moins 3 caractères', maxMessage: 'Le nom de la compétence ne peut pas contenir plus de 255 caractères')]
+    #[Assert\Regex(pattern: '/^[a-zA-Zé]+$/', message: 'Le nom de la compétence ne peut contenir que des lettres')]
     private ?string $nom = null;
 
     #[ORM\Column(length: 3)]
+    #[Assert\NotBlank(message: 'Le nom de la stat ne peut pas être vide')]
+    #[Assert\Length(min: 3, max: 3,minMessage: 'Le nom de la stat doit contenir au moins 3 caractères', maxMessage: 'Le nom de la stat ne peut pas contenir plus de 3 caractères')]
+    #[Assert\Regex(pattern: '/^[a-zA-Zé]+$/', message: 'Le nom de la stat ne peut contenir que des lettres')]
     private ?string $stat = null;
 
     #[ORM\OneToMany(mappedBy: 'competence', targetEntity: CompetencePersonnage::class)]
